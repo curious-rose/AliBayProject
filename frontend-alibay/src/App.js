c66d30f9a3451ec72d59ea2acc7aedbde5f2c992
Timothy Zulkarnain <TZ@Timothys-MacBook-Air.local>
2017-11-07 16:27:12 -0500

    merge

--- frontend-alibay/src/App.js
+++ frontend-alibay/src/App.js
@@ -2,32 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import AccountPage from './components/AccountPage'
 import CreateAccount from './components/CreateAccount'
-<<<<<<< HEAD
-import ItemPage from './components/ItemPage'
-=======
->>>>>>> 00ec5f9bce0ca0f7a196e8ce69ea85fbcce8b3e4
 import LandingPage from './components/LandingPage'
 import SellStuff from './components/SellStuff'
 import SearchResults from './components/SearchResults'
 import ShopAll from './components/ShopAll'
 import SignIn from './components/SignIn'
-<<<<<<< HEAD
-import { Link, Route } from 'react-router-dom'
-import NavBar from './components/NavBar'
-=======
 import NavBar from './components/NavBar.js'
 // import ItemPage from './components/ItemPage'
 import { BrowserRouter, Link, Route } from 'react-router-dom'
 
->>>>>>> 00ec5f9bce0ca0f7a196e8ce69ea85fbcce8b3e4
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
-          <SellStuff />
-=======
           <NavBar/>
           <BrowserRouter>
             <div>
@@ -41,7 +29,6 @@ class App extends Component {
             </div>
           </BrowserRouter>
           
->>>>>>> 00ec5f9bce0ca0f7a196e8ce69ea85fbcce8b3e4
 
       </div>
     );
