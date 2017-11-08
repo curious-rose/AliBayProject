dd6ecba756736544aeb5983a89f883bd062c8a78
Timothy Zulkarnain <TZ@Timothys-MacBook-Air.local>
2017-11-08 15:42:15 -0500

    t

--- frontend-alibay/src/components/AccountPage.js
+++ frontend-alibay/src/components/AccountPage.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
-import backendFunctions from '../backend-mockup.js';
+import { allItemsSold, allListings, allItemsBought } from '../backend-mockup.js';
 
 class AccountPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            items: []
+        };
     }
+    displayItem = (x) => (<li> {x} </li>)
 
-    
     render() {
         return (
             <div className="AccountPage">
-                <h1>Your Account. Welcome username </h1>
+                <h1>Your Account. Welcome (Enter username)</h1>
                 <Link to="/shopall" className="shopButton"><button>Shop now!</button></Link>
                 <Link to="/sellstuff" className="sellSomething"><button>Sell Something!</button></Link>
                 <div className="accountAction">
                     <div className="ItemsPurchased">
                         <h2>Items Purchased</h2>
                         <ul>
-                            <li>test1</li>
+                            {this.state.items.map(this.displayItem)}
                         </ul>
                     </div>
                     <div className="ItemsSold">
--- frontend-alibay/src/components/ShopAll.js
+++ frontend-alibay/src/components/ShopAll.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 
 class ShopAll extends Component {
     constructor(props) {
